1e5f572540325c25514d3eb6eedf8523c5d4af91
Chris <17653365+odcambc@users.noreply.github.com>
2026-01-16 17:07:44 -0600

    plugin split off

--- fusilli-multiqc/fusilli_multiqc/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-FUSILLI MultiQC modules.
-
-This package contains the following MultiQC modules:
-- detection: Detection efficiency, library coverage, and sensitivity metrics
-- diversity: Library diversity metrics (Shannon, Simpson, evenness)
-- preprocessing: Read retention and loss through preprocessing steps
-- partners: Partner detection metrics and coverage
-"""
